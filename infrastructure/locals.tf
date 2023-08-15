1d3d9ac9c61c86973f800e51108859613f192aef
Leticia Valladares <leticia.valladares@cloudreach.com>
2023-08-15 09:12:24 +0200

    Increase the size for web server from t3 medium to large

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -132,7 +132,7 @@ locals {
       user_data = "user-data-sqlite.sh"
     },
     app-server-mysql = {
-      type      = "t3.medium"
+      type      = "t3.large"
       user_data = "user-data-mysql.sh"
     }
   }
