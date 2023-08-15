a425b6af55d03f02847cab1988b1f9b745996562
Leticia Valladares <leticia.valladares@cloudreach.com>
2023-08-15 10:22:35 +0200

    Resize db ans change name

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -1,9 +1,9 @@
 resource "aws_db_instance" "rds" {
   allocated_storage    = 10
-  db_name              = "users"
+  db_name              = "juiceshop"
   engine               = "mysql"
   engine_version       = "8.0.32"
-  instance_class       = "db.t2.small"
+  instance_class       = "db.t3.small"
   username             = jsondecode(nonsensitive(data.aws_secretsmanager_secret_version.db_secrets.secret_string))["MYSQL_USER"]
   password             = jsondecode(nonsensitive(data.aws_secretsmanager_secret_version.db_secrets.secret_string))["MYSQL_PASSWORD"]
   skip_final_snapshot  = true
